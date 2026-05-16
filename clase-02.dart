bf2ea4344c165db7497c2f11feb07ff04ddf4d5f
estu75717642-ui <estu75717642@institutobolivar.edu.pe>
2026-05-15 21:55:53 -0500

    Evidencias

---
+++ clase-02.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num1 = 8;
+  int num2 = 12;
+
+  int suma = num1 + num2;
+
+  print("La suma es: $suma");
+
+  int lado = 5;
+
+  int area = lado * lado;
+
+  print("El área del cuadrado es: $area");
+}
